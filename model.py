a2a7e4952e3fd5b6ca7953ff6f536abae7e599eb
Hyperform99 <cb130703@gmail.com>
2026-09-12 22:09:33 -0400

    Step 7: decay_epsilon

--- model.py
+++ model.py
@@ -55,8 +55,9 @@ def epsilon_greedy_action(q_table, state, epsilon, action_space, rng):
 #Get all indexes
 #Select a random one
 
-# Step 7 - decay_epsilon (not yet solved)
-# TODO: implement
+# Step 7 - decay_epsilon
+def decay_epsilon(epsilon, decay_rate, min_epsilon):
+    return(max(min_epsilon, epsilon * decay_rate))
 
 # Step 8 - td_target (not yet solved)
 # TODO: implement
